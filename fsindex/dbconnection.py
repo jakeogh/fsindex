7adc4f4274388e0115ba8f7d84606c258183bfef
jakeogh <github.com@v6y.net>
2017-02-17 01:53:54 +0000

    auto-commit

--- fsindex/dbconnection.py
+++ fsindex/dbconnection.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
 import sqlite3
+print("sqlite3.connect()")
 conn = sqlite3.connect('/poolz3_8x5TB_A/__fsindex/_good/fsindex.sha1.db')
+print("conn.cursor()")
 c = conn.cursor()
