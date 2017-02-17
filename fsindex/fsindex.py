398694a202521f04863f74614b8622e1d8ded9b4
jakeogh <github.com@v6y.net>
2017-02-17 23:48:51 +0000

    auto-commit

--- fsindex/fsindex.py
+++ fsindex/fsindex.py
@@ -140,6 +140,7 @@ def search(ctx, field, term, resultfields, exists, substring, modes):
     seprint("exists:", exists)
     seprint("substring:", substring)
     print(MODES.keys())
+    print(list(MODES.keys()))
     for mode in modes:
         print("mode:", mode)
         assert mode in MODES.keys()
