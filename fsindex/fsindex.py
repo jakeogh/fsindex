f04ea1557bab1b089f013b1731b842e573840be2
jakeogh <github.com@v6y.net>
2017-02-18 06:28:28 +0000

    auto-commit

--- fsindex/fsindex.py
+++ fsindex/fsindex.py
@@ -29,12 +29,10 @@ CONTEXT_SETTINGS = \
 
 @click.group(chain=True)
 def cli():
-    """This script processes a bunch of images through pillow in a unix
-    pipe.  One commands feeds into the next.
+    """Interface to fsindex.
     Example:
     \b
-        imagepipe open -i example01.jpg resize -w 128 display
-        imagepipe open -i example02.jpg blur save
+        fsindex search --field file_name --term test.txt display
     """
 
 @cli.resultcallback()
@@ -43,6 +41,7 @@ def process_commands(processors):
     subcommands.  As in this example each subcommand returns a function
     we can chain them together to feed one into the other, similar to how
     a pipe on unix works.
+    From: https://github.com/pallets/click/blob/master/examples/imagepipe/imagepipe.py
     """
     # Start with an empty iterable.
     stream = ()
@@ -58,6 +57,7 @@ def process_commands(processors):
 def processor(f):
     """Helper decorator to rewrite a function so that it returns another
     function from it.
+    From: https://github.com/pallets/click/blob/master/examples/imagepipe/imagepipe.py
     """
     def new_func(*args, **kwargs):
         def processor(stream):
@@ -65,10 +65,10 @@ def processor(f):
         return processor
     return update_wrapper(new_func, f)
 
-
 def generator(f):
     """Similar to the :func:`processor` but passes through old values
     unchanged and does not pass through the values as parameter.
+    From: https://github.com/pallets/click/blob/master/examples/imagepipe/imagepipe.py
     """
     @processor
     def new_func(stream, *args, **kwargs):
@@ -77,8 +77,6 @@ def generator(f):
         for item in f(*args, **kwargs):
             yield item
     return update_wrapper(new_func, f)
-
-
 
 @cli.command('search')
 @click.option('--field', required=True, nargs=1, type=click.Choice(list(FIELDS.keys())))
@@ -178,122 +176,14 @@ def stats():
         print(thing)
     quit(0)
 
+@cli.command()
+@click.argument('root', required=True, nargs=1)
+def update(root):
+    update_db(root)
+    quit(0)
 
 
 
-#
-#def match_field(field, term, resultfields, exists, substring, modes):
-#    if 'hash' in field:
-#        term = term.lower()
-#    if substring:
-#        query = '''SELECT * FROM path_db WHERE ''' + field + ''' LIKE ?'''
-#        answer = c.execute(query, (b'%'+term+b'%',))
-#    else:
-#        query = '''SELECT * FROM path_db WHERE ''' + field + '''=?'''
-#        answer = c.execute(query, (term,))
-#
-#    results = answer.fetchall()
-#
-#    filtered_count = 0
-#    for result in results:
-#        if exists:
-#            if not path_exists(result[1]):
-#                continue
-#        valid_mode = False
-#        if modes:
-#            if matching_mode(result, modes):
-#                valid_mode = True
-#        if modes:
-#            if not valid_mode:
-#                continue
-#
-#        newline = False
-#        for index, rfield in enumerate(FIELDS.keys()):
-#            if rfield in resultfields or not resultfields:
-#                newline = True
-#                if isinstance(result[index], bytes):
-#                    sys.stdout.buffer.write(result[index] + b' ')
-#                else:
-#                    print(result[index], end=' ')
-#        if newline:
-#            filtered_count += 1
-#            print('\n', end='')
-#            newline = False
-#
-#    count = len(results)
-#    seprint("original count:", "{:,}".format(count))
-#    seprint("filtered_count:", "{:,}".format(filtered_count))
-#    seprint("\n", end='')
-#
-#
-#@click.group(context_settings=CONTEXT_SETTINGS)
-#@click.option('--verbose', is_flag=True, callback=set_verbose, expose_value=False)
-#@click.pass_context
-#def fsindex(ctx):
-#    pass
-#
-#@fsindex.command()
-#@click.argument('root', required=True, nargs=1)
-#@click.pass_context
-#def update(root):
-#    update_db(root)
-#
-#
-#
-#@fsindex.command()
-#@click.argument('results', required=True, nargs=-1, type=click.File('rb'))
-#@click.option('--exists', is_flag=True)
-#@click.option('--mode', is_flag=False, nargs=1,
-#              type=click.Choice(list(MODE_FUNCTIONS.keys())),
-#              required=False, multiple=True)
-#@click.pass_context
-#def filter(ctx, results, exists, mode):
-#
-#
-#    for result in results:
-#        print("filter():", result)
-#
-#
-#@fsindex.command()
-#@click.argument('field', required=True, nargs=1)
-#@click.argument('term', required=True, nargs=1)
-#@click.argument('resultfields', required=False, nargs=-1)
-#@click.option('--exists', is_flag=True)
-#@click.option('--substring', is_flag=True)
-#@click.option('--mode', is_flag=False, nargs=1,
-#              type=click.Choice(list(MODE_FUNCTIONS.keys())),
-#              required=False, multiple=True)
-#@click.option('--verbose', is_flag=True)
-#@click.pass_context
-#def old_search(ctx, field, term, resultfields, exists, substring, mode, verbose):
-#    modes = mode
-#    assert field in FIELDS.keys()
-#    if verbose: seprint("field:", field)
-#    if verbose: seprint("term:", term)
-#    for rfield in resultfields:
-#        assert rfield in FIELDS.keys()
-#    if verbose: seprint("resultfields:", resultfields)
-#    if verbose: seprint("exists:", exists)
-#    if verbose: seprint("substring:", substring)
-#    assert len(modes) <= len(MODE_FUNCTIONS.keys())
-#    for mode in modes:
-#        #print("mode:", mode)
-#        assert mode in MODE_FUNCTIONS.keys()
-#    if verbose: seprint("modes:", modes)
-#
-#    if FIELDS[field] == 'BLOB':
-#        term = bytes(term, 'UTF8')
-#    elif FIELDS[field] == 'INT':
-#        term = int(term)
-#    match_field(field=field, term=term, resultfields=resultfields, exists=exists, substring=substring, modes=modes)
-#
-#if __name__ == '__main__':
-#    # pylint: disable=no-value-for-parameter
-#    fsindex()
-#    # pylint: enable=no-value-for-parameter
-#    #eprint("Exiting without error.", level=LOG['DEBUG'])
-#
-#
 #def search_existing_file_hash(infile):
 #    infile = bytes(infile, 'UTF8')
 #    infile = os.path.realpath(infile)
