5ad65b1d8ea5569813699d012e9b5b61dce76034
jakeogh <github.com@v6y.net>
2017-02-18 05:46:21 +0000

    auto-commit

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
     install_requires=dependencies,
     entry_points={
         'console_scripts': [
-            'fsindex = fsindex.fsindex:fsindex',
+            'fsindex = fsindex.fsindex:cli',
         ],
     },
     classifiers=[
