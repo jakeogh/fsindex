a330b45867425e1794df59dbb213e92bfb2bfde6
jakeogh <github.com@v6y.net>
2017-02-19 00:14:16 +0000

    auto-commit

--- fsindex/fsindex.py
+++ fsindex/fsindex.py
@@ -53,10 +53,9 @@ def process_commands(processors):
         stream = processor(stream)
 
     # Evaluate the stream and throw away the items.
-    print(type(stream))
-    print(stream)
-    for _ in stream:
-        pass
+    if stream:
+        for _ in stream:
+            pass
 
 def processor(f):
     """Helper decorator to rewrite a function so that it returns another
