e1286d4938106759dc83da6f59c52751ae51706a
jakeogh <github.com@v6y.net>
2017-12-23 03:07:46 -0700

    auto-commit

--- fsindex/cli/list_objects/list_objects.py
+++ fsindex/cli/list_objects/list_objects.py
@@ -4,7 +4,7 @@ import click
 from kcl.printops import eprint
 from iridb.model.Tag import Tag
 from kcl.sqlalchemy.self_contained_session import self_contained_session
-from fsindex.cli.list_objects.filerecords import filerecords
+from fsindex.cli.list_objects.filerecord import filerecord
 
 @click.group()
 def list_objects():
