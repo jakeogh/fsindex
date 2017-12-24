ad2a52bd8c92c9e506c398888c43d8df913492d5
jakeogh <github.com@v6y.net>
2017-12-23 20:11:49 -0700

    auto-commit

--- fsindex/fsindex.py
+++ fsindex/fsindex.py
@@ -10,6 +10,7 @@ from kcl.sqlalchemy.test import test as kcltest
 from kcl.sqlalchemy.print_database import print_database
 from kcl.sqlalchemy.self_contained_session import self_contained_session
 from kcl.sqlalchemy.BaseMixin import BASE
+from kcl.sqlalchemy.ipython import ipythoin
 from kcl.sqlalchemy.model.Config import CONFIG
 from kcl.click.CONTEXT_SETTINGS import CONTEXT_SETTINGS
 from .cli.list_objects.list_objects import list_objects
@@ -50,10 +51,10 @@ def test(package, keep_databases, count, test_class, test_match):
 fsindex.add_command(list_objects, name='list')
 fsindex.add_command(create_objects, name='create')
 fsindex.add_command(print_database)
+fsindex.add_command(ipython)
 
 #fsindex.add_command(bookmark)
 #fsindex.add_command(content_files)
 #fsindex.add_command(show_config, name='config')
 #fsindex.add_command(display_database)
-#fsindex.add_command(ipython)
 #fsindex.add_command(import_all_iris)
