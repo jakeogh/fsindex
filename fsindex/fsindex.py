c5dee936dd3aa218f9ddda23de572040c0024556
jakeogh <github.com@v6y.net>
2017-12-23 23:25:42 -0700

    auto-commit

--- fsindex/fsindex.py
+++ fsindex/fsindex.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
 import click
+import sys
+print(sys.argv[:])
 import shutil
 from sqlalchemy_utils.functions import database_exists
 from sqlalchemy_utils.functions import create_database
