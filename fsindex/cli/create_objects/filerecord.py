90aad5ad83caf259736f68c9402060e0e86ec13f
jakeogh <github.com@v6y.net>
2017-12-23 19:16:41 -0700

    auto-commit

--- fsindex/cli/create_objects/filerecord.py
+++ fsindex/cli/create_objects/filerecord.py
@@ -5,8 +5,11 @@ from kcl.sqlalchemy.self_contained_session import self_contained_session
 from kcl.sqlalchemy.model.FileRecord import FileRecord
 from kcl.sqlalchemy.BaseMixin import BASE
 
+
+# exists=False or cant pass broken symlinks
+# dir_okay=True or cant pass dirs or symlinks to dirs
 @click.command()
-@click.argument('path', type=click.Path(exists=True, dir_okay=True, path_type=bytes, allow_dash=False), nargs=1)
+@click.argument('path', type=click.Path(exists=False, dir_okay=True, path_type=bytes, allow_dash=False), nargs=1)
 @click.pass_obj
 def filerecord(config, path):
     with self_contained_session(config.database) as session:
