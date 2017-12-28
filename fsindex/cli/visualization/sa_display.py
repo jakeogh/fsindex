e53004c6a482fbe842dfc61b611dfa8e2478bebb
jakeogh <github.com@v6y.net>
2017-12-27 20:06:53 -0700

    auto-commit

--- fsindex/cli/visualization/sa_display.py
+++ fsindex/cli/visualization/sa_display.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 import click
-from kcl.sqlalchemy.model import FileRecord
+from kcl.sqlalchemy.model.FileRecord import FileRecord
 from kcl.sqlalchemy.visualization.sa_display import sa_display as kcl_sa_display
 
 @click.command()
