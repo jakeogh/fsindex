b0a067c4a3b2d9e8345678d3afeff05a86c0e056
jakeogh <github.com@v6y.net>
2017-12-28 02:56:05 -0700

    auto-commit

--- fsindex/fsindex.py
+++ fsindex/fsindex.py
@@ -13,7 +13,37 @@ from .cli.list_objects.list_objects import list_objects
 from .cli.create_objects.create_objects import create_objects
 
 fsindex.help = CONFIG.appname + " interface"
+
+@fsindex.command()
+@click.option('--package', is_flag=False, type=str, required=False, default='fsindex')
+@click.option('--keep-databases', is_flag=True)
+@click.option('--count', is_flag=False, type=int, required=False)
+@click.option('--test-class', is_flag=False, type=str, required=False)
+@click.option('--test-match', is_flag=False, type=str, required=False)
+def test(package, keep_databases, count, test_class, test_match):
+    kcltest(package=package, keep_databases=keep_databases, count=count, test_class=test_class, test_match=test_match)
+
+
+@fsindex.command()
+@click.option('--table', type=str, default=False)
+@click.pass_context
+def print_database(ctx, table):
+    ctx.invoke(kcl_print_database, database=ctx.obj.database, table=table)
+
+
+fsindex.add_command(list_objects, name='list')
+fsindex.add_command(create_objects, name='create')
+#fsindex.add_command(print_database)
 fsindex.add_command(ipython)
+fsindex.add_command(sa_display)
+
+#fsindex.add_command(bookmark)
+#fsindex.add_command(show_config, name='config')
+#fsindex.add_command(display_database)
+#fsindex.add_command(import_all_iris)
+
+
+
 
 
 
@@ -72,33 +102,3 @@ fsindex.add_command(ipython)
 #    pass
 
 
-
-
-@fsindex.command()
-@click.option('--package', is_flag=False, type=str, required=False, default='fsindex')
-@click.option('--keep-databases', is_flag=True)
-@click.option('--count', is_flag=False, type=int, required=False)
-@click.option('--test-class', is_flag=False, type=str, required=False)
-@click.option('--test-match', is_flag=False, type=str, required=False)
-def test(package, keep_databases, count, test_class, test_match):
-    kcltest(package=package, keep_databases=keep_databases, count=count, test_class=test_class, test_match=test_match)
-
-
-#@click.pass_obj
-@fsindex.command()
-@click.option('--table', type=str, default=False)
-@click.pass_context
-def print_database(ctx, table):
-    ctx.invoke(kcl_print_database, database=ctx.obj.database, table=table)
-
-
-fsindex.add_command(list_objects, name='list')
-fsindex.add_command(create_objects, name='create')
-#fsindex.add_command(print_database)
-#fsindex.add_command(ipython)
-fsindex.add_command(sa_display)
-
-#fsindex.add_command(bookmark)
-#fsindex.add_command(show_config, name='config')
-#fsindex.add_command(display_database)
-#fsindex.add_command(import_all_iris)
