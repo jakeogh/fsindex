1836af965d6e86d6baedb0ab7af90cbd7e455836
jakeogh <github.com@v6y.net>
2017-12-27 19:28:18 -0700

    q

--- fsindex/cli/visualization/sa_display.py
+++ fsindex/cli/visualization/sa_display.py
@@ -2,9 +2,8 @@
 
 import click
 from kcl.sqlalchemy.model import FileRecord
-from kcl.sqlalchemy.visualization import sa_display as kcl_sa_display
+from kcl.sqlalchemy.visualization.sa_display import sa_display as kcl_sa_display
 
 @click.command()
 def sa_display():
     kcl_sa_display()
-
