f7320aaea398a81ad8a9c3372f053ff3c03242e4
jakeogh <github.com@v6y.net>
2017-12-27 20:12:28 -0700

    auto-commit

--- fsindex/cli/visualization/sa_display.py
+++ fsindex/cli/visualization/sa_display.py
@@ -7,4 +7,4 @@ from kcl.sqlalchemy.visualization.sa_display import sa_display as kcl_sa_display
 @click.command()
 def sa_display():
     import IPython; IPython.embed()
-    kcl_sa_display()
+    kcl_sa_display(globals())
