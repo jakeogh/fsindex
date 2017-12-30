a6f687a14d9f7458e6594014ea67b004705cb2ee
jakeogh <github.com@v6y.net>
2017-12-29 21:45:25 -0700

    auto-commit

--- fsindex/fsindex.py
+++ fsindex/fsindex.py
@@ -6,28 +6,20 @@ from fsindex.model.Config import CONFIG
 builtins.config = CONFIG
 
 from kcl.sqlalchemy.clickapp.clickapp import clickapp as fsindex
+fsindex.help = CONFIG.appname + " interface"
 
 from kcl.sqlalchemy.clickapp.default import *
-#from kcl.sqlalchemy.clickapp.print_database import print_database
-#from kcl.sqlalchemy.clickapp.test import test
-#from kcl.sqlalchemy.clickapp.show_config import show_config
-#from kcl.sqlalchemy.ipython import ipython
-
+fsindex.add_command(ipython)
+fsindex.add_command(print_database)
+fsindex.add_command(test)
+fsindex.add_command(show_config, name="config")
 
 from .cli.visualization.sa_display import sa_display
 from .cli.list_objects.list_objects import list_objects
 from .cli.create_objects.create_objects import create_objects
 
-fsindex.help = CONFIG.appname + " interface"
-
+fsindex.add_command(sa_display)
 fsindex.add_command(list_objects, name='list')
 fsindex.add_command(create_objects, name='create')
-fsindex.add_command(ipython)
-fsindex.add_command(sa_display)
-fsindex.add_command(print_database)
 
-#fsindex.add_command(show_config, name='config')
 #fsindex.add_command(display_database)
-fsindex.add_command(test)
-fsindex.add_command(show_config, name="config")
-
