06ea6cb25a99e8e6a23828160d24546cdbfdf77c
jakeogh <github.com@v6y.net>
2017-12-29 21:40:56 -0700

    auto-commit

--- fsindex/fsindex.py
+++ fsindex/fsindex.py
@@ -6,10 +6,14 @@ from fsindex.model.Config import CONFIG
 builtins.config = CONFIG
 
 from kcl.sqlalchemy.clickapp.clickapp import clickapp as fsindex
-from kcl.sqlalchemy.clickapp.print_database import print_database
-from kcl.sqlalchemy.clickapp.test import test
-from kcl.sqlalchemy.clickapp.show_config import show_config
-from kcl.sqlalchemy.ipython import ipython
+
+from kcl.sqlalchemy.clickapp.default import *
+#from kcl.sqlalchemy.clickapp.print_database import print_database
+#from kcl.sqlalchemy.clickapp.test import test
+#from kcl.sqlalchemy.clickapp.show_config import show_config
+#from kcl.sqlalchemy.ipython import ipython
+
+
 from .cli.visualization.sa_display import sa_display
 from .cli.list_objects.list_objects import list_objects
 from .cli.create_objects.create_objects import create_objects
