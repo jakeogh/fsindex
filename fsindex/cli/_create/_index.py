e6789cd5a1883d1a320d32c321d85e16b3f2ba8b
jakeogh <github.com@v6y.net>
2018-01-10 03:33:00 -0700

    auto-commit

--- fsindex/cli/_create/_index.py
+++ fsindex/cli/_create/_index.py
@@ -24,10 +24,10 @@ def _index(config, paths, verbose):
             BASE.metadata.create_all(session.bind)
             pathlib_object = pathlib.Path(os.fsdecode(path))
             for index, path in enumerate(all_files_iter(pathlib_object)):
-                print(' ')
-                ceprint("index path:", path)
+                #print(' ')
+                #ceprint("index path:", path)
                 filerecord = FileRecord.construct(session=session, path=bytes(path), verbose=verbose)
                 session.add(filerecord)
-                #if index % 100:
-                session.flush()
-                session.commit()
+                if index % 100:
+                    session.flush()
+                    session.commit()
