4a886411b5d4d83c336234d2fbce61d5c3e8725c
jakeogh <github.com@v6y.net>
2018-02-21 19:33:14 -0700

    auto-commit

--- fsindex/test/tests/Path/level2_path.py
+++ fsindex/test/tests/Path/level2_path.py
@@ -3,13 +3,11 @@
 from fsindex.test.test_enviroment import *
 with self_contained_session(CONFIG.database_timestamp) as session:
     BASE.metadata.create_all(session.bind)
-
     path = Path.construct(session=session, path=b"/level1/level2")
     session.add(path)
     session.commit()
 
 db_result = [('select COUNT(*) from filename;', 3),
-             ('select COUNT(*) from path;', 1),
-             ('select COUNT(*) from pathfilename;', 3)]
+             ('select COUNT(*) from path;', 3)]
 
 check_db_result(config=CONFIG, db_result=db_result)
